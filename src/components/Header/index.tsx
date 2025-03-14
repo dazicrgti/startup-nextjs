b1de359312c2906580667b8a8b8626c7de14ca1d
dazicrgti <dazi.cremonita@gti.com.hk>
2025-03-14 15:43:00 +0800

    logo

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -49,7 +49,7 @@ const Header = () => {
       >
         <div className="container">
           <div className="relative -mx-4 flex items-center justify-between">
-            <div className="w-60 max-w-full px-4 xl:mr-12">
+            <div className="w-60 max-w-8/10 px-4 xl:mr-12">
               <Link
                 href="/"
                 className={`header-logo block w-full ${
@@ -59,14 +59,14 @@ const Header = () => {
                 <Image
                   src="/images/logo/logo-2.png"
                   alt="logo"
-                  width={100}
+                  width={140}
                   height={30}
                   className="w-full dark:hidden"
                 />
                 <Image
                   src="/images/logo/logo.png"
                   alt="logo"
-                  width={100}
+                  width={140}
                   height={30}
                   className="hidden w-full dark:block"
                 />
